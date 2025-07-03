7ee624b6fd356e1fceb94b879b27484c5d922c27
Ludvig Böklin <ludvig.boklin@proton.me>
2025-07-03 16:18:16 +0200

    fix error after https://github.com/NixOS/nixpkgs/pull/410417

--- default.nix
+++ default.nix
@@ -1,34 +1,45 @@
-{ lib, stdenv, optipng, cairo, python3Packages, pkg-config,
-pngquant, which, imagemagick }:
+{
+  lib,
+  stdenv,
+  optipng,
+  cairo,
+  nototools,
+  python3Packages,
+  pkg-config,
+  pngquant,
+  which,
+  imagemagick,
+}: let
+  version = "2019-07-01-original-blobs";
+in
+  stdenv.mkDerivation {
+    name = "noto-fonts-emoji-ogblobs";
 
-let version = "2019-07-01-original-blobs";
-in stdenv.mkDerivation {
-  name = "noto-fonts-emoji-ogblobs";
+    src = ./.;
 
-  src = ./.;
+    buildInputs = [cairo];
+    nativeBuildInputs =
+      [pngquant optipng which cairo pkg-config imagemagick nototools]
+      ++ (with python3Packages; [python fonttools]);
 
-  buildInputs = [ cairo ];
-  nativeBuildInputs = [ pngquant optipng which cairo pkg-config imagemagick ]
-                  ++ (with python3Packages; [ python fonttools nototools ]);
+    postPatch = ''
+      sed -i 's,^PNGQUANT :=.*,PNGQUANT := ${pngquant}/bin/pngquant,' Makefile
+      patchShebangs flag_glyph_name.py
+    '';
 
-  postPatch = ''
-    sed -i 's,^PNGQUANT :=.*,PNGQUANT := ${pngquant}/bin/pngquant,' Makefile
-    patchShebangs flag_glyph_name.py
-  '';
+    enableParallelBuilding = true;
 
-  enableParallelBuilding = true;
+    installPhase = ''
+      mkdir -p $out/share/fonts/noto
+      cp NotoColorEmoji.ttf $out/share/fonts/noto
+    '';
 
-  installPhase = ''
-    mkdir -p $out/share/fonts/noto
-    cp NotoColorEmoji.ttf $out/share/fonts/noto
-  '';
-
-  meta = with lib; {
-    inherit version;
-    description = "Color and Black-and-White emoji fonts";
-    homepage = "https://github.com/googlei18n/noto-emoji";
-    license = with licenses; [ ofl asl20 ];
-    platforms = platforms.all;
-    maintainers = with maintainers; [ mathnerd314 ];
-  };
-}
+    meta = with lib; {
+      inherit version;
+      description = "Color and Black-and-White emoji fonts";
+      homepage = "https://github.com/googlei18n/noto-emoji";
+      license = with licenses; [ofl asl20];
+      platforms = platforms.all;
+      maintainers = with maintainers; [mathnerd314];
+    };
+  }
